8aa6fbb9c4e12fddf9c3570553731b1e7308ac00
Lucas Oliveira <lucashmo@gmail.com>
2020-03-16 23:03:44 -0300

    Criando repositorio para estudar javascript

---
+++ readme.txt
@@ -0,0 +1,7 @@
+15/03/2020
+Aprendendo as formas de se usar o javascript.
+Vendo o console do navegador e como ele funciona.
+
+16/03/2020
+Testando as quatro formas de output.
+Iniciando o aprendizado de variaveis.
---
+++ script.js
@@ -0,0 +1,22 @@
+/* Formas de Outputs
+document.getElementById("titulo").innerHTML = "Opa, tudo bem ?"
+
+document.write("Algum Texto")
+
+window.alert("Xesque")
+
+console.log("Dale dele dele Doly")*/
+
+var nome ="Lucas"
+var sobrenome = "Henrique"
+var idade = 90;
+var x = 10;
+var y = 15;
+
+
+var nomecompleto = nome+" "+sobrenome;
+var total = x + y;
+
+alert(nomecompleto);
+alert(idade);
+alert(total);
